3790883d35402cca5a40407955aa824d3c961d66
Leonard Lesinski <leonard.lesinski@tu-dortmund.de>
2025-12-09 09:29:28 +0100

    added "work in progress"

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="bg-black text-white dark w-full min-h-screen">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+	<title>Surreal</title>
+</svelte:head>
+
+<div class="w-full min-h-screen flex items-center justify-center flex-col gap-12">
+	<div class="flex items-center gap-6">
+		<img src="/surreal.svg" alt="Icon" class="h-22 rounded-2xl border-neutral-700 border" />
+		<h1 class="font-semibold text-6xl">Surreal.</h1>
+	</div>
+	<p class="font-light text-lg">work in progress, stay tuned 🏗</p>
+</div>
