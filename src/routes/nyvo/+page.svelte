f38f68fe36af667f68cb88a14d3608cfedf07088
Leonard Lesinski <leonard.lesinski@tu-dortmund.de>
2025-12-10 13:20:18 +0100

    Added Nyvo page

--- src/routes/nyvo/+page.svelte
+++ src/routes/nyvo/+page.svelte
@@ -1,11 +1,14 @@
 <svelte:head>
-	<title>Surreal</title>
+	<title>Nyvo | Surreal</title>
 </svelte:head>
 
 <div class="w-full min-h-screen flex items-center justify-center flex-col gap-12">
 	<div class="flex items-center gap-6">
-		<img src="/surreal.svg" alt="Surreal icon" class="h-22 rounded-2xl border-neutral-700 border" />
-		<h1 class="font-semibold text-6xl">Surreal</h1>
+		<img src="/nyvo.svg" alt="Surreal icon" class="h-22 rounded-2xl border-neutral-700 border" />
+		<h1 class="font-semibold text-6xl">
+			Surreal
+			<span class="font-light">Nyvo</span>
+		</h1>
 	</div>
 	<p class="font-light text-lg">work in progress, stay tuned 🏗</p>
 </div>
